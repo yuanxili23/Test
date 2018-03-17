42e980d5db939bcd9f9b48f4ec4b6ab475272046
Yuanxi Li <yuanxi.li@rutgers.edu>
2018-03-17 12:12:20 -0700

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = "Test",
+    version = "0.0.4",
+    author = "Yuanxi Li",
+    author_email = "yuanxi.li@rutgers.edu",
+    description = ("An demonstration of how to create, document, and publish "
+                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=[],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
