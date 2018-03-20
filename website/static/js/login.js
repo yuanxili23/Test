17f859bc4bf7fc3776fa572022f9bc72baace0d0
Yuanxi Li <yuanxi.li@rutgers.edu>
2018-03-20 08:57:39 -0700

    Extends template from layout.html

--- website/static/js/login.js
+++ website/static/js/login.js
@@ -1,45 +1,48 @@
-$('.message a').click(function(){
-    $('div.form-page').animate({height: "toggle", opacity: "toggle"}, "slow");
+jQuery(document).ready(function($){
+    //you can now use $ as your jQuery object.
+    $('.message a').click(function(){
+        $('div.form-page').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+
+    $('button[id=create]').click(function(){
+        var user = $('div.register-form input[id=register-user]').val();
+        var password = $('div.register-form input[id=register-password]').val();
+        $.ajax({
+            url: '/user/register',
+            data: {'user': user, 'password': password},
+            type: 'POST',
+            success: function(response) {
+                window.location.response;
+            },
+            error: function(error) {
+                if (error.status == 400) {
+                    window.location = "error/400.html";
+                } else {
+                    console.log(error);
+                    window.location = "error/unknown.html";
+                }
+            }
+        });
+    })
+
+    $('button[id=login]').click(function(){
+        var user = $('div.login-form input[id=login-user]').val();
+        var password = $('div.login-form input[id=login-password]').val();
+        $.ajax({
+            url: '/user/login',
+            data: {'user': user, 'password': password},
+            type: 'POST',
+            success: function(response) {
+                window.location = response;
+            },
+            error: function(error) {
+                if (error.status == 400) {
+                    window.location = "error/400.html";
+                } else {
+                    console.log(error);
+                    window.location = "error/unknown.html";
+                }
+            }
+        });
+    })
 });
-
-$('button[id=create]').click(function(){
-    var user = $('div.register-form input[id=register-user]').val();
-    var password = $('div.register-form input[id=register-password]').val();
-    $.ajax({
-        url: '/user/register',
-        data: {'user': user, 'password': password},
-        type: 'POST',
-        success: function(response) {
-            window.location.response;
-        },
-        error: function(error) {
-            if (error.status == 400) {
-                window.location = "error/400.html";
-            } else {
-                console.log(error);
-                window.location = "error/unknown.html";
-            }
-        }
-    });
-})
-
-$('button[id=login]').click(function(){
-    var user = $('div.login-form input[id=login-user]').val();
-    var password = $('div.login-form input[id=login-password]').val();
-    $.ajax({
-        url: '/user/login',
-        data: {'user': user, 'password': password},
-        type: 'POST',
-        success: function(response) {
-            window.location = response;
-        },
-        error: function(error) {
-            if (error.status == 400) {
-                window.location = "error/400.html";
-            } else {
-                console.log(error);
-                window.location = "error/unknown.html";
-            }
-        }
-    });
-})
